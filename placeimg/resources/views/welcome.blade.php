939ffe75aa0b5ec397ab378c4c3c9baccfe69fd0
zamronypj <zamronypj@yahoo.com>
2016-01-14 09:56:19 +0700

    Edit view resources/views/welcome.blade.php
    Add Bootstrap CSS and JS to view and add some reformat and English translation

--- placeimg/resources/views/welcome.blade.php
+++ placeimg/resources/views/welcome.blade.php
@@ -2,14 +2,27 @@
 <html>
     <head>
         <title>Image place holder</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     </head>
     <body>
-        <h1>Image place holder</h1>
-        <p>Untuk menggunakan layanan ini, sertakan kode berikut dalam file HTML:</p>
-        <p><code>&lt;img src=&quot;http://place.img/[width]x[height]/[format]&quot; &gt;</code> di mana <em>[width]</em>,  <em>[height]</em> dan <em>[format]</em>, masing-masing adalah nilai integer panjang, lebar dan format gambar yang diinginkan. Nilai <em>[format]</em> yang diizinkan yakni <em>jpg</em> atau <em>png</em>. Jika <em>[format]</em> tidak ditentukan, nilai defaultnya adalah <em>jpg</em></p>
-        <p><code>&lt;img src=&quot;http://place.img/320x240&quot; &gt;</code></p>
-        <img src="{{ url('320x240') }}" >
-        <p><code>&lt;img src=&quot;http://place.img/200x200/png&quot; &gt;</code></p>
-        <img src="{{ url('200x200/png') }}" >
+	   <div class="container">
+	    <div class="jumbotron">
+         <h1>Image place holder</h1>
+		</div>
+		<div class="row">
+		  <div class="col-sm-12">
+              <p>To use this service, include following code inside a HTML document:</p>
+              <p><code>&lt;img src=&quot;http://place.img/[width]x[height]/[format]&quot; &gt;</code> where <em>[width]</em>,  <em>[height]</em> and <em>[format]</em>, are width, height and format of image requested. Allowed value for <em>[format]</em> are <em>jpg</em> and <em>png</em>. If <em>[format]</em> is not defined, default value is <em>jpg</em></p>
+              <p><code>&lt;img src=&quot;http://place.img/320x240&quot; &gt;</code></p>
+              <img src="{{ url('320x240') }}" >
+              <p><code>&lt;img src=&quot;http://place.img/200x200/png&quot; &gt;</code></p>
+              <img src="{{ url('200x200/png') }}" >
+		   </div>
+		</div> 
+	   </div>	
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+       <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>			
     </body>
 </html>
